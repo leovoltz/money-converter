3c913c3b26e201a2f431d879b861df4bccd5dec3
Leonardo Pereira <ethical.leonardo@gmail.com>
2023-12-20 14:43:41 -0300

    configurado formatação dos valores ao converter

--- scripts.js
+++ scripts.js
@@ -2,10 +2,19 @@ const button = document.getElementById('convert-button')
 
 const dolar = 4.86
 
-const convertValues = () =>{
-     const inputReais = document.getElementById('input-real').value
-     
-     console.log(inputReais / dolar)
+const convertValues = () => {
+    const inputReais = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+    const currencyValueText = document.getElementById('currency-value-text')
+
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: "BRL" }
+    ).format(inputReais)
+
+
+    currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+        { style: 'currency', currency: "USD" }
+    ).format(inputReais / dolar)
 }
 
 button.addEventListener('click', convertValues)
